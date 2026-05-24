5dab9ce2da6e0433a50aacad0d300aded8458884
Eliezer Lucas <eliezerlucaslg@gmail.com>
2026-05-24 15:05:10 -0300

    feat: mapper implementado

---
+++ src/main/kotlin/br/com/fiap/oficina/presentation/mapper/ServicoMapper.kt
@@ -0,0 +1,22 @@
+package br.com.fiap.oficina.presentation.mapper
+
+import br.com.fiap.oficina.domain.entity.Servico
+import br.com.fiap.oficina.presentation.dto.ServicoDto
+import org.mapstruct.Mapping
+import org.mapstruct.factory.Mappers
+
+abstract class ServicoMapper {
+    companion object {
+        val INSTANCE: ServicoMapper = Mappers.getMapper(ServicoMapper::class.java)
+    }
+
+    @Mapping(source = "cliente.id", target = "cliente")
+    abstract fun toResponse(servico: Servico): ServicoDto
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "cliente", ignore = true)
+    @Mapping(target = "funcionario", ignore = true)
+    @Mapping(target = "pecas", ignore = true)
+    abstract fun toEntity(dto: ServicoDto): Servico
+
+}
