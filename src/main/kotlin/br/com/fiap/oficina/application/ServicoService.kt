d67df946d6f6afad755d11e8788c0ba6b1a7f06b
Eliezer Lucas <eliezerluca@gmail.com>
2026-05-24 16:07:24 -0300

    fix: anotacao service adicionada e associacao com cliente no salvamento.

--- src/main/kotlin/br/com/fiap/oficina/application/ServicoService.kt
+++ src/main/kotlin/br/com/fiap/oficina/application/ServicoService.kt
@@ -3,13 +3,20 @@ package br.com.fiap.oficina.application
 import br.com.fiap.oficina.domain.entity.Servico
 import br.com.fiap.oficina.domain.repository.ClienteRepository
 import br.com.fiap.oficina.domain.repository.ServicoRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
+@Service
 class ServicoService (
     private val repository: ServicoRepository,
     private val clienteRepository: ClienteRepository
 ) {
 
-    fun salvar(servico: Servico) : Servico {
+    @Transactional
+    fun salvar(servico: Servico, clienteId: Long) : Servico {
+        val cliente = clienteRepository.findById(clienteId)
+            .orElseThrow { IllegalArgumentException("Cliente não encontrado com o ID: $clienteId") }
+        servico.cliente = cliente
         return repository.save(servico)
     }
 
@@ -21,6 +28,7 @@ class ServicoService (
         return repository.findAll()
     }
 
+    @Transactional
     fun deletarPorId(id: Long) : String? {
         if(!repository.existsById(id)){
             throw IllegalArgumentException("Serviço não encontrado para deletar.")
