70666de34864721783d014be009492b922a59bef
Eliezer Lucas <eliezerluca@gmail.com>
2026-05-24 15:29:04 -0300

    feat: roles e documentação Swagger acrescentadas.

--- src/main/kotlin/br/com/fiap/oficina/presentation/controller/ServicoController.kt
+++ src/main/kotlin/br/com/fiap/oficina/presentation/controller/ServicoController.kt
@@ -2,6 +2,9 @@ package br.com.fiap.oficina.presentation.controller;
 
 import br.com.fiap.oficina.application.ServicoService;
 import br.com.fiap.oficina.presentation.mapper.ServicoMapper;
+import io.swagger.v3.oas.annotations.Operation
+import jakarta.annotation.security.RolesAllowed
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/servicos")
@@ -10,14 +13,44 @@ public class ServicoController(
     private val mapper:ServicoMapper,
 ) {
 
+    @PostMapping
+    @RolesAllowed("ATENDENTE", "ADMIN")
+    @Operation(
+        summary = "Criar um novo servico",
+        description = "Cadastra um novo servico no sistema"
+    )
     fun criar(){}
 
+    @PostMapping("/{id}")
+    @RolesAllowed("ATENDENTE", "ADMIN")
+    @Operation(
+        summary = "Atualizar um servico",
+        description = "Atualiza um servico no sistema"
+    )
     fun atualizar(){}
 
+    @GetMapping("/{id}")
+    @RolesAllowed("ATENDENTE", "ADMIN")
+    @Operation(
+        summary = "Listar servico por ID",
+        description = "Lista um servico do sistema pelo ID"
+    )
     fun listarPorId(){}
 
+    @GetMapping()
+    @RolesAllowed("ATENDENTE", "ADMIN")
+    @Operation(
+        summary = "Listar servicos",
+        description = "Lista todos os servicos"
+    )
     fun listarTodos(){}
 
+    @DeleteMapping("/{id}")
+    @RolesAllowed("ATENDENTE", "ADMIN")
+    @Operation(
+        summary = "Deletar servico por ID",
+        description = "Deleta um servico do sistema pelo ID"
+    )
     fun deletarPorId(){}
 
 }
