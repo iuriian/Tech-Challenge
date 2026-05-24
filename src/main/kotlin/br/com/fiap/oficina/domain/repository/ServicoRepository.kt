f32693f90acd5726551df3808859686f104aae58
Eliezer Lucas <eliezerluca@gmail.com>
2026-05-24 16:07:20 -0300

    fix: anotacao repository adicionada ao servico repository.

--- src/main/kotlin/br/com/fiap/oficina/domain/repository/ServicoRepository.kt
+++ src/main/kotlin/br/com/fiap/oficina/domain/repository/ServicoRepository.kt
@@ -1,7 +1,8 @@
 package br.com.fiap.oficina.domain.repository
 
-import br.com.fiap.oficina.domain.entity.Cliente
 import br.com.fiap.oficina.domain.entity.Servico
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
-interface ServicoRepository : JpaRepository<Servico, Long> {}
+@Repository
+interface ServicoRepository : JpaRepository<Servico, Long>
