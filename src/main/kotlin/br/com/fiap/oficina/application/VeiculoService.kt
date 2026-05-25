35af0f3128e7cf11a6985c046ccc2e9ed2f4bb92
Nisk-Moraes <nicolasmoraes1906@yahoo.com.br>
2026-05-25 06:40:12 -0300

    adicao de parte das funcionalidades de veiculo

---
+++ src/main/kotlin/br/com/fiap/oficina/application/VeiculoService.kt
@@ -0,0 +1,32 @@
+package br.com.fiap.oficina.application
+
+import br.com.fiap.oficina.domain.entity.Cliente
+import br.com.fiap.oficina.domain.entity.Veiculo
+import br.com.fiap.oficina.domain.repository.VeiculoRepository
+import br.com.fiap.oficina.presentation.mapper.VeiculoMapper
+import org.springframework.stereotype.Service
+
+@Service
+class VeiculoService(
+    private val repository: VeiculoRepository,
+    private val mapper: VeiculoMapper
+) {
+    fun salvarVeiculo(veiculo: Veiculo): Veiculo {
+        if (repository.existsByPlaca(veiculo.placa)){
+            throw IllegalArgumentException("Veiculo já cadastrado")
+        }
+        return repository.save(veiculo)
+    }
+
+    fun buscarPorId(idVeiculo: Long): Veiculo? {
+        return repository.findByIdVeiculo(idVeiculo)
+    }
+
+    fun buscarPorPlaca(placa: String): Veiculo? {
+        return repository.findByPlaca(placa)
+    }
+
+    fun buscarPorMotorista(motorista: Cliente): List<Veiculo> {
+        return repository.findByMotorista(motorista)
+    }
+}
---
+++ src/main/kotlin/br/com/fiap/oficina/presentation/dto/VeiculoDTO.kt
@@ -0,0 +1,34 @@
+package br.com.fiap.oficina.presentation.dto
+
+import br.com.fiap.oficina.domain.entity.Cliente
+import jakarta.validation.constraints.Size
+
+data class VeiculoDTO(@Size(min=3, max=20) val nome: String,
+                      val marca: String,
+                      val modelo: String,
+                      val ano: String,
+                      @Size(min = 7, max = 7) val placa: String,
+                      val motorista: Cliente? = null){}
+
+
+/* oficina/domain/entity/Veiculo.kt
+*   @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var idVeiculo: Long? = null
+
+    @Column(nullable = false)
+    lateinit var nome: String
+
+    @Column(nullable = false)
+    lateinit var modelo: String
+
+    @Column(nullable = false)
+    lateinit var ano: String
+
+    @Column(nullable = false, unique = true)
+    @Size(min = 7, max = 7, message = "Input must be exactly 7 characters long")
+    lateinit var placa: String
+
+    @Column(nullable = false, unique = true)
+    lateinit var motorista: Cliente
+* */
