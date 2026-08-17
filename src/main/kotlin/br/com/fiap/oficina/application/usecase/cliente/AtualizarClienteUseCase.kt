3aa80d9f20ffe3bf720f6fdfd8417e4614765cf8
Eliezer Lucas <eliezerlucaslg@gmail.com>
2026-08-17 20:29:34 -0300

    refactor: remover ClienteService e adicionar AtualizarClienteUseCase

--- src/main/kotlin/br/com/fiap/oficina/application/usecase/cliente/AtualizarClienteUseCase.kt
+++ src/main/kotlin/br/com/fiap/oficina/application/usecase/cliente/AtualizarClienteUseCase.kt
@@ -1,11 +1,10 @@
 package br.com.fiap.oficina.application.usecase.cliente
+
 import br.com.fiap.oficina.application.port.out.ClienteRepository
 import br.com.fiap.oficina.domain.entity.Cliente
 import org.springframework.stereotype.Service
 
 @Service
-class BuscarClientePorNomeUseCase(
-    private val clienteRepository: ClienteRepository
-) {
-    fun executar(nome: String): Cliente? = clienteRepository.buscarPorNome(nome)
+class AtualizarClienteUseCase(private val clienteRepository: ClienteRepository) {
+    fun executar(cliente: Cliente): Cliente = clienteRepository.salvar(cliente)
 }
