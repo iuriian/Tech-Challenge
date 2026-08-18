8b2965c5b2e5faf9de1cad3f58a11123bf507b45
Iuri Ian <iuriian@hotmail.com>
2026-08-18 10:41:58 -0300

    implementacao crud pecas

---
+++ src/main/kotlin/br/com/fiap/oficina/domain/repository/PecaRepository.kt
@@ -0,0 +1,20 @@
+package br.com.fiap.oficina.domain.repository
+
+import br.com.fiap.oficina.domain.entity.Peca
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface PecaRepository : JpaRepository<Peca, Long> {
+
+    fun findAllByAtivoTrue(): List<Peca>
+
+    fun findByCodigoAndAtivoTrue(codigo: String): Peca?
+
+    fun findByNomeIgnoreCaseAndAtivoTrue(nome: String): Peca?
+
+    fun existsByCodigoAndAtivoTrue(codigo: String): Boolean
+
+    fun findByCodigo(codigo: String): Peca?
+
+    fun existsByCodigo(codigo: String): Boolean
+
+}
