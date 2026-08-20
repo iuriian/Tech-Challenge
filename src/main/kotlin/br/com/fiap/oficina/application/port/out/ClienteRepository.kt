71545297c3c0ced340bbf22437c9e148f2e0f0fc
Eliezer Lucas <eliezerlucaslg@gmail.com>
2026-08-20 19:47:12 -0300

    refactor: mover ClienteRepository para application.port.out

--- src/main/kotlin/br/com/fiap/oficina/application/port/out/ClienteRepository.kt
+++ src/main/kotlin/br/com/fiap/oficina/application/port/out/ClienteRepository.kt
@@ -1,4 +1,4 @@
-package br.com.fiap.oficina.domain.repository
+package br.com.fiap.oficina.application.port.out
 
 import br.com.fiap.oficina.domain.entity.Cliente
 import br.com.fiap.oficina.domain.valueobject.Id
