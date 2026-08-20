b02a48d6db17bb8109faa4956365360cbd41929c
Eliezer Lucas <eliezerlucaslg@gmail.com>
2026-08-20 20:08:37 -0300

    feat: adicionar CriarClienteUseCase

---
+++ src/main/kotlin/br/com/fiap/oficina/application/usecase/cliente/CriarClienteUseCase.kt
@@ -0,0 +1,12 @@
+package br.com.fiap.oficina.application.usecase.cliente
+import br.com.fiap.oficina.application.port.out.ClienteRepository
+import br.com.fiap.oficina.domain.entity.Cliente
+import org.springframework.stereotype.Service
+
+@Service
+class CriarClienteUseCase(
+    private val clienteRepository: ClienteRepository
+) {
+    fun executar(cliente: Cliente): Cliente = clienteRepository.salvar(cliente)
+
+}
