d6ae69d12305345e6cf4024841eb8c6f0d8f60d2
Alan Alves <dev.alan87@gmail.com>
2026-08-23 18:07:42 -0300

    refactor: separa ordem de servico e generaliza orcamento

---
+++ src/main/kotlin/br/com/fiap/oficina/domain/enum/OrdemServicoStatus.kt
@@ -0,0 +1,11 @@
+package br.com.fiap.oficina.domain.enum
+
+enum class OrdemServicoStatus(val descricao: String) {
+    RECEBIDA("Recebida"),
+    EM_DIAGNOSTICO("Em Diagnostico"),
+    AGUARDANDO_APROVACAO("Aguardando aprovação"),
+    EM_EXECUCAO("Em Execução"),
+    FINALIZADA("Finalizada"),
+    ENTREGUE("Entregue"),
+    CANCELADA("Cancelada pelo cliente"),
+}
---
+++ src/main/kotlin/br/com/fiap/oficina/domain/enum/TipoItemOrcamento.kt
@@ -0,0 +1,6 @@
+package br.com.fiap.oficina.domain.enum
+
+enum class TipoItemOrcamento {
+    SERVICO,
+    PECA,
+}
