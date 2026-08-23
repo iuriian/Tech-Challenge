d5f30e17b6541dc927c820d238e75b4893fff4d7
Alan Alves <dev.alan87@gmail.com>
2026-08-23 20:59:50 -0300

    feat: adiciona dominio e casos de uso de servico

--- src/main/kotlin/br/com/fiap/oficina/domain/entity/Servico.kt
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.oficina.domain.entity
-
-@Deprecated(
-    "Use OrdemServico",
-    replaceWith = ReplaceWith("OrdemServico"),
-)
-typealias Servico = OrdemServico
--- src/main/kotlin/br/com/fiap/oficina/domain/repository/ServicoRepository.kt
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.oficina.domain.repository
-
-@Deprecated(
-    "Use OrdemServicoRepository",
-    replaceWith = ReplaceWith("OrdemServicoRepository"),
-)
-typealias ServicoRepository = OrdemServicoRepository
