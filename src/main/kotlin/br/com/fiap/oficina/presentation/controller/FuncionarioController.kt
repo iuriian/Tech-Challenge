9fbdeb75af20c1e510cc3a97c1271027236cfde3
Eliezer Lucas <eliezerlucaslg@gmail.com>
2026-08-24 21:03:45 -0300

    fix: executar ações via service, e não usecase.

--- src/main/kotlin/br/com/fiap/oficina/presentation/controller/FuncionarioController.kt
+++ src/main/kotlin/br/com/fiap/oficina/presentation/controller/FuncionarioController.kt
@@ -1,14 +1,7 @@
 package br.com.fiap.oficina.presentation.controller
 
-import br.com.fiap.oficina.domain.usecase.funcionario.AtualizarFuncionarioUseCase
-import br.com.fiap.oficina.domain.usecase.funcionario.BuscarFuncionarioPorIdUseCase
-import br.com.fiap.oficina.domain.usecase.funcionario.BuscarFuncionarioPorNomeUseCase
-import br.com.fiap.oficina.domain.usecase.funcionario.CriarFuncionarioUseCase
-import br.com.fiap.oficina.domain.usecase.funcionario.ListarFuncionariosUseCase
-import br.com.fiap.oficina.domain.usecase.funcionario.RemoverFuncionarioUseCase
-import br.com.fiap.oficina.domain.valueobject.Id
+import br.com.fiap.oficina.application.service.FuncionarioService
 import br.com.fiap.oficina.presentation.dto.FuncionarioDto
-import br.com.fiap.oficina.presentation.mapper.FuncionarioMapper
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.tags.Tag
@@ -26,19 +19,12 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/funcionarios")
 @Tag(name = "Funcionários", description = "Operações relacionadas ao gerenciamento de funcionários")
 class FuncionarioController(
-    private val criarFuncionarioUseCase: CriarFuncionarioUseCase,
-    private val listarFuncionariosUseCase: ListarFuncionariosUseCase,
-    private val buscarFuncionarioPorIdUseCase: BuscarFuncionarioPorIdUseCase,
-    private val buscarFuncionarioPorNomeUseCase: BuscarFuncionarioPorNomeUseCase,
-    private val atualizarFuncionarioUseCase: AtualizarFuncionarioUseCase,
-    private val removerFuncionarioUseCase: RemoverFuncionarioUseCase,
-    private val mapper: FuncionarioMapper,
+    val funcionarioService: FuncionarioService,
 ) {
     @PostMapping
     @Operation(summary = "Criar um novo funcionário", description = "Cadastra um novo funcionário no sistema")
     fun cadastrar(@Valid @RequestBody funcionarioDto: FuncionarioDto): FuncionarioDto {
-        val entity = mapper.toEntity(funcionarioDto)
-        return mapper.toResponse(criarFuncionarioUseCase.executar(entity))
+        return funcionarioService.cadastrar(funcionarioDto)
     }
 
     @GetMapping
@@ -46,22 +32,21 @@ class FuncionarioController(
         summary = "ListarListar todos os funcionários",
         description = "Retorna uma lista com todos os funcionários cadastrados",
     )
-    fun listarTodos(): List<FuncionarioDto> = listarFuncionariosUseCase.executar().map { mapper.toResponse(it) }
+    fun listarTodos(): List<FuncionarioDto> = funcionarioService.listarTodos()
 
     @GetMapping("/id/{id}")
     @Operation(
         summary = "Buscar funcionário por ID",
         description = "Busca um funcionário através do seu identificador único",
     )
-    fun buscarPorId(@PathVariable id: String): FuncionarioDto? =
-        buscarFuncionarioPorIdUseCase.executar(Id.fromString(id))?.let { mapper.toResponse(it) }
+    fun buscarPorId(@PathVariable id: String): FuncionarioDto? = funcionarioService.buscarPorId(id)
 
     @GetMapping("/nome/{nome}")
     @Operation(summary = "Buscar funcionário por nome", description = "Busca um funcionário através do seu nome")
     fun buscarPorNome(
         @Parameter(description = "Nome do funcionário", required = true, example = "Vini Jr.")
         @PathVariable nome: String,
-    ): FuncionarioDto? = buscarFuncionarioPorNomeUseCase.executar(nome)?.let { mapper.toResponse(it) }
+    ): FuncionarioDto? = funcionarioService.buscarPorNome(nome)
 
     @PutMapping("/{id}")
     @Operation(
@@ -69,8 +54,7 @@ class FuncionarioController(
         description = "Atualiza as informações de um funcionário existente",
     )
     fun alterar(@PathVariable id: String, @Valid @RequestBody funcionarioDto: FuncionarioDto): FuncionarioDto {
-        val entity = mapper.toEntityComId(id, funcionarioDto)
-        return mapper.toResponse(atualizarFuncionarioUseCase.executar(entity))
+        return funcionarioService.editar(id, funcionarioDto)
     }
 
     @DeleteMapping("/{id}")
@@ -78,5 +62,5 @@ class FuncionarioController(
         summary = "Deletar um funcionário",
         description = "Remove um funcionário do sistema através do seu identificador único",
     )
-    fun deletar(@PathVariable id: String) = removerFuncionarioUseCase.executar(Id.fromString(id))
+    fun deletar(@PathVariable id: String) = funcionarioService.deletar(id)
 }
