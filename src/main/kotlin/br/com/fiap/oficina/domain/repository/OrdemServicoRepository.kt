4a48fdb0a080a7a6bf17e8e1e0ab87f0f7ed9ede
Alan Alves <dev.alan87@gmail.com>
2026-08-25 00:14:09 -0300

    feat: adiciona filtros ao repositorio de ordens de servico

--- src/main/kotlin/br/com/fiap/oficina/domain/repository/OrdemServicoRepository.kt
+++ src/main/kotlin/br/com/fiap/oficina/domain/repository/OrdemServicoRepository.kt
@@ -1,7 +1,9 @@
 package br.com.fiap.oficina.domain.repository
 
 import br.com.fiap.oficina.domain.entity.OrdemServico
+import br.com.fiap.oficina.domain.enum.OrdemServicoStatus
 import br.com.fiap.oficina.domain.valueobject.Id
+import java.time.Instant
 
 interface OrdemServicoRepository {
     fun salvar(ordemServico: OrdemServico): OrdemServico
@@ -10,7 +12,13 @@ interface OrdemServicoRepository {
 
     fun listarTodos(): List<OrdemServico>
 
+    fun listarPorStatus(status: OrdemServicoStatus): List<OrdemServico>
+
     fun listarPorCliente(clienteId: Id): List<OrdemServico>
+
+    fun listarPorVeiculo(veiculoId: Id): List<OrdemServico>
+
+    fun listarPorDataAberturaEntre(inicio: Instant, fim: Instant): List<OrdemServico>
 
     fun existePorId(id: Id): Boolean
 
