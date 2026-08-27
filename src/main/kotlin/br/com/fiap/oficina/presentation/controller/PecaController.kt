a7e1cffe2fa9d6ec605667b7ddeee14230c9bf70
Eliezer Lucas <eliezerlucaslg@gmail.com>
2026-08-27 07:04:21 -0300

    fix(controller): retornar 200 OK nos endpoints DELETE
    Remove @ResponseStatus(NO_CONTENT) de Cliente, Peça, Serviço e Veículo
    para alinhar com o padrão Funcionário e arq_proposta.md; testes já
    esperam 200.

--- src/main/kotlin/br/com/fiap/oficina/presentation/controller/PecaController.kt
+++ src/main/kotlin/br/com/fiap/oficina/presentation/controller/PecaController.kt
@@ -9,7 +9,6 @@ import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.annotation.security.RolesAllowed
 import jakarta.validation.Valid
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
@@ -20,7 +19,6 @@ import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 
