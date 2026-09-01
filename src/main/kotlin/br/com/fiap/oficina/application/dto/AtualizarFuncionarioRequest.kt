21f6afe3c6d39da7c6bbc747592184577a2c97fb
Eliezer Lucas <eliezerlucaslg@gmail.com>
2026-08-31 23:53:14 -0300

    refactor(funcionario): unificar CriarFuncionarioRequest e AtualizarFuncionarioRequest em FuncionarioRequest

--- src/main/kotlin/br/com/fiap/oficina/application/dto/AtualizarFuncionarioRequest.kt
+++
@@ -1,3 +0,0 @@
-package br.com.fiap.oficina.application.dto
-
-data class AtualizarFuncionarioRequest(val nome: String, val cargo: String)
--- src/main/kotlin/br/com/fiap/oficina/application/dto/CriarFuncionarioRequest.kt
+++
@@ -1,3 +0,0 @@
-package br.com.fiap.oficina.application.dto
-
-data class CriarFuncionarioRequest(val nome: String, val cargo: String)
